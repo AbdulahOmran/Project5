bc3de2391dedb0f33a554bc976fc1cc9421ec651
Abdulah Omran <abdulah.omran@patelhs.org>
2021-02-26 13:28:47 +0000

    Added First Method

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
+
 class Main {
   public static void main(String[] args) {
+    
+
 System.out.println("For today, we're going to have a large increase in customers, so count it for corporate");
 System.out.println("Okay!");
  System.out.println("An hour passes and not many customers come");
 int result = sum(2);
 System.out.println("1st Hour Customer Count: " + result);
-  }
   public static int sum(int num){
     if(num>0){
       //if I recieve 5x as num then it becomes 5x+4x+3x+2x+x
@@ -15,7 +17,7 @@ System.out.println("1st Hour Customer Count: " + result);
       //3+3=6
       //2+1=3
       //1+0=1
-    }else{
+    }else{ 
       return 0;
         
     }
